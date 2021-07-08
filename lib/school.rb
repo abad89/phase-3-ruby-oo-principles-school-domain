3b8169d508fc86a4be1d8da1990b8af64d5721c5
abad89 <abadalamenti89@gmail.com>
2021-07-07 22:23:35 -0400

    completed assignment

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+
+class School
+    attr_accessor :roster
+    attr_reader :name
+
+    def initialize(name, roster={})
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(name, grade)
+        roster[grade] ||= []
+        roster[grade] << name
+    end
+
+    def grade(student_grade)
+        roster[student_grade]
+    end
+
+    def sort
+        sorted_roster = {}
+        roster.each do |grade, students|
+            sorted_roster[grade] = students.sort
+        end
+        sorted_roster
+    end
+end
